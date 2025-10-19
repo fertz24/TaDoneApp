064730dfa1f98ddbc983c0501a974685d3098f5e
Fernanda Garcia <briannafernanda4@gmail.com>
2025-10-19 15:23:56 -0700

    comentando el codigo

--- lib/vista/home_vista.dart
+++ lib/vista/home_vista.dart
@@ -11,9 +11,10 @@ class HomeVista extends StatefulWidget {
   
 class _HomeVistaState extends State<HomeVista> { 
   
-  
-    int _botonIndice = 0;
-    final _widgets = [
+    int _botonIndice = 0; //variable que indica que el boton esta activo
+    //0 = pendientes, 1 = completadas
+
+    final _widgets = [ //lista de widgets
       
       //Widget para tareas pendientes
       Container(), 
@@ -31,36 +32,36 @@ class _HomeVistaState extends State<HomeVista> {
         backgroundColor: Color(0xFF1d2630),
         foregroundColor: Colors.white,
         title: Text("To Do"),
-        actions: [
+        actions: [ //Boton para cerrar sesion donde, al presionarlo se cerrara la sesion con Firebase
           IconButton(
             onPressed: () async {
               await FirebaseAuth.instance.signOut(); //para salir de la sesion
               Navigator.pushReplacement(context,
                   MaterialPageRoute(builder: (context) => LoginVista())); //una vez que sale entonces, se redirige al loginVista
             },
-            icon: Icon(Icons.exit_to_app),
+            icon: Icon(Icons.exit_to_app), //icono de salir de la sesion
           )
         ],
       ),
-      body: SingleChildScrollView(
+      body: SingleChildScrollView( //permite desplazamiento vertical
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             SizedBox(height: 20),
-            Row(
+            Row( //boton para tareas pendientes
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 InkWell(
                   onTap: () {
                     setState(() {
-                      _botonIndice = 0;
+                      _botonIndice = 0; //cuando se presiona el boton para tareas pendientes, el indice cambia a 0
                     });
                   },
-                  child: Container(
+                  child: Container( //estilo del boton
                     height: 50,
                     width: MediaQuery.of(context).size.width / 2.2,
                     decoration: BoxDecoration(
-                      color: _botonIndice == 0 ? Colors.indigo : Colors.white,
+                      color: _botonIndice == 0 ? Colors.indigo : Colors.white, //cambia el color y tamaño (si esta activo)
                       borderRadius: BorderRadius.circular(10),
                     ),
                     child: Center(
