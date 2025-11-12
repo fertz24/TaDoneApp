5e582445a5b61223039a503c474c50e6e18a62a7
GARCIA BORBOA BRIANNA FERNANDA <briannafernanda4@gmail.com>
2025-11-12 11:56:29 -0800

    agregando el deslizar para marcar tarea completada y eliminar

--- lib/vista/pendientes_vista.dart
+++ lib/vista/pendientes_vista.dart
@@ -1,7 +1,10 @@
 import 'package:fer1/modelo/todo_modelo.dart';
 import 'package:fer1/presentador/create_todo_presentador.dart';
+import 'package:fer1/presentador/delete_todo_presentador.dart';
+import 'package:fer1/presentador/update_todo_presentador.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class PendientesVista extends StatefulWidget {
   const PendientesVista({super.key});
@@ -15,6 +18,8 @@ class _PendientesVistaState extends State<PendientesVista> {
   late String uid; //Declaramos una variable late la cual se inicializará después
 
   final CreateTodoPresentador _createTodoPresentador = CreateTodoPresentador(); //Instancia del presentador para poder manejar la lógica de agregar tareas
+  final UpdateTodoPresentador _updateTodoPresentador = UpdateTodoPresentador(); //Instancia del update_presentador para manejar la lógica de editar tareas
+  final DeleteTodoPresentador _deleteTodoPresentador = DeleteTodoPresentador(); //Instancia del delete_presentador para eliminar las tareas
 
   @override
   void initState() {
@@ -44,28 +49,60 @@ class _PendientesVistaState extends State<PendientesVista> {
                   color: Colors.white, 
                   borderRadius: BorderRadius.circular(10),
                 ),
+                
+                child: Slidable( //Permite dezlizar horizontalmente un elemento (la tarea) para poder mostrar las acciones de editar/eliminar
+                  key: ValueKey(todo.id), //Se asigna una clave única con todo.id, lo cual ayuda a Flutter el identificar y manejar de forma correcta el Widget cuando se reconstruye la interfaz (se evitan errores visuales o de estado)
+                  endActionPane: ActionPane(
+                    motion: DrawerMotion(), 
+                    children: [
+                      SlidableAction(
+                        backgroundColor: Colors.green,
+                        foregroundColor: Colors.white,
+                        icon: Icons.done,
+                        label: "Mark",
+                        onPressed: (context) {
+                        _updateTodoPresentador.editarTodoEstado(todo.id, true);
+                      })
+                    ],
+                  ),
+                  startActionPane: ActionPane(
+                    motion: DrawerMotion(), 
+                    children: [
+                      SlidableAction(
+                        backgroundColor: Colors.amber,
+                        foregroundColor: Colors.white,
+                        icon: Icons.edit,
+                        label: "Edit",
+                        onPressed: (context) {
+                        _updateTodoPresentador.editarTodoEstado(todo.id, true);
+                      }),
+                      SlidableAction(
+                        backgroundColor: Colors.red,
+                        foregroundColor: Colors.white,
+                        icon: Icons.delete,
+                        label: "Delete",
+                        onPressed: (context) async{
+                          await _deleteTodoPresentador.eliminarTodo(todo.id);
+                      })
+                    ],
+                  ),
                   child: ListTile( //Usamos ListTile, este es un widget que organiza el contenido en una fila
                   //es ideal ya que representaremos cada tarea como una tarjeta simple y ordenada
                     title: Text(
                       todo.titulo, //Título de la tarea
                       style: TextStyle(
-                        fontWeight: FontWeight.w500, 
-                      ),
+                        fontWeight: FontWeight.w500),
                     ),
-                    subtitle: Text(
-                      todo.descripcion, //Descipción de la tarea
-                    ),
-                    trailing: Text(
-                      '${dt.day}/${dt.month}/${dt.year}', //Formato de fecha
+                    subtitle: Text(todo.descripcion), //Descipción de la tarea
+                    trailing: Text('${dt.day}/${dt.month}/${dt.year}', //Formato de fecha
                       style: TextStyle(
-                        fontWeight: FontWeight.bold,
+                        fontWeight: FontWeight.bold),
                     ),
                   ),
                 ),
-              
-            );
-          },
-        );
+              );
+            },
+          );
         } else {
           return Center(child: CircularProgressIndicator(color: Colors.white)); //Circulo girando (efecto) para indicar que está cargando las tareas
         }
