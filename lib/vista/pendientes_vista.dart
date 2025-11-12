2f9b779519f3418005a03649260d9da6429b2217
GARCIA BORBOA BRIANNA FERNANDA <briannafernanda4@gmail.com>
2025-11-12 12:42:51 -0800

    agregando la vista para editar la tarea o sea funcion de mostrarTarea

--- lib/vista/pendientes_vista.dart
+++ lib/vista/pendientes_vista.dart
@@ -75,7 +75,9 @@ class _PendientesVistaState extends State<PendientesVista> {
                         icon: Icons.edit, //Ícono de lápiz para editar tarea
                         label: "Edit",
                         onPressed: (context) {
-                        _updateTodoPresentador.editarTodoEstado(todo.id, true); //dudas
+                        _mostrarTarea(context, todo: todo); //Llamamos a la función de _mostrarTarea y el objeto todo, 
+                        //el cual nos ayuda a abrir el popup de edición con los datos de esa tarea (todo) ya cargados para asi poder editar dicha tarea
+
                       }),
                       SlidableAction(
                         backgroundColor: Colors.red,
@@ -107,6 +109,84 @@ class _PendientesVistaState extends State<PendientesVista> {
         } else {
           return Center(child: CircularProgressIndicator(color: Colors.white)); //Circulo girando (efecto) para indicar que está cargando las tareas
         }
+      },
+    );
+  }
+
+  void _mostrarTarea(BuildContext context, {TodoModelo? todo}) { //Función del onPressed
+    //el segundo parámetro es para que la función sepa si debe mostrar una tarea existente para poder editarla
+    //o en el caso contrario es crear una nueva tarea (todo es null), esta función sirve para ambos casos
+
+      //Dentro de los paréntesis se asigna un valor inicial al controlador del texto, si todo NO es null entonces se usa todo.titulo/descripcion (editar tarea) o SI es null entonces el campo queda vacío (crear tarea)
+      final TextEditingController _tituloControlador = TextEditingController(text: todo?.titulo); //Creamos un controlador para poder capturar lo que el usuario escribe en los campos de título y descripción
+      final TextEditingController _descripcionControlador = TextEditingController(text: todo?.descripcion); //Para la descripción de la tarea
+      final CreateTodoPresentador _presentador = CreateTodoPresentador(); //Instanciamos la clase del create_todo_presentador para poder llamar a crearTodo si el usuario guarda la tarea
+      final UpdateTodoPresentador _update = UpdateTodoPresentador(); //Instanciamos la clase de update_todo_presentador para llamar a update para editar la tarea
+
+      showDialog( //Para el popup
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            backgroundColor: Colors.white,
+            title: Text(todo == null ? "Add Task" : "Edit Task", //If ternario para decidir que texto mostrar
+            //Si todo es null entonces significa que se esta creando una nueva tarea (Add Task) o si todo tiene datos entonces se está editando una tarea existente (Edit Task)
+
+            style: TextStyle(
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          content: SingleChildScrollView( //permitimos que el contenido del popup sea desplazable (este evita errores de desbordamiento en pantallas pequeñas)
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              child: Column( //Organiza widgets en forma vertical
+                children: [
+                  TextField(
+                    controller: _tituloControlador,
+                    decoration: InputDecoration(
+                      labelText: "Title", 
+                      border: OutlineInputBorder(), //dibuja un borde rectangular con esquinas redondeadas alrededor de un campo de texto
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                    TextField(
+                    controller: _descripcionControlador,
+                    decoration: InputDecoration(
+                      labelText: "Description", 
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          actions: [
+            TextButton(onPressed: () {
+              Navigator.pop(context);
+            }, 
+            child: Text("Cancel"),
+            ),
+            ElevatedButton( //Botón para guardar la tarea
+            style: ElevatedButton.styleFrom( //Estilo del botón Add
+              backgroundColor: Colors.indigo, 
+              foregroundColor: Colors.white,
+            ),
+              onPressed: () async {
+                if(todo == null) { //Verificamos si no se pasó una tarea existente
+                //Si es null significa que se está creando una nueva tarea
+
+                  await _presentador.crearTodo( //Llamamos al método crearTodo del presentador, enviando el texto que el usuario escribio en los campos de...
+                  _tituloControlador.text, //título de la tarea (captura el texto)
+                  _descripcionControlador.text); //descripción de la tarea
+                } else {
+                  //En else, si hay una tarea entonces se está editando una ya existente llamando al editarTodo usando el id y los nuevos valores
+                  await _update.editarTodo(todo.id, _tituloControlador.text, _descripcionControlador.text);
+                }
+                Navigator.pop(context); //Cierra el popup después de guardar la tarea
+              },
+              child: Text(todo == null ? "Add" : "Update"), //Mostrará el texto "Add" si todo es null (crear tarea) o "Update" si todo tiene datos (editar tarea)
+            )
+          ],
+        );
       },
     );
   }
